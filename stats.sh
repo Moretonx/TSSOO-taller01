bdb479f1ceedb3784df31e968d925ce70d558a67
Francisco <francisco.moretti@alumnos.cl>
2020-06-05 03:17:06 +0000

    actualizacion archivo stats.sh

--- stats.sh
+++ stats.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
+
+# La variable $# es equiv a argc
+if [ $# != 1 ]; then
+        echo "Uso: $0 <directorio busqueda>"
+        exit
+fi
+
+searchDir=$1
+
+# Falta verificar que searchDir es realmente un directorio
+if [ ! -e $searchDir ]; then
+        echo "Directorio $1 no existe"
+        exit
+fi
+
+printf "Directorio busqueda: %s\n" $1
 
