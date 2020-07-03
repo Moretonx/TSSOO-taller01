b088625aa8858f1c2064e9648f415e3bf0de17f2
Francisco <francisco.moretti@alumnos.cl>
2020-07-03 09:15:40 +0000

    Archivos finales

--- README.md
+++ README.md
@@ -1,7 +1,27 @@
 ## Autor
-
 Francisco Moretti Castro
 
 ## Correo institucional
-
 francisco.moretti@alumnos.uv.cl
+
+Para poder buscar los archivos de simulación de debe ingresar la ubicación del directorio por parametro al archivo stats.sh.  
+
+En la búsqueda de archivos específicos se hace de la siguiente manera:  
+
+    `find $searchDir -name '*NombreArchivo*.txt' -print | sort | grep -v '._'`
+
+Para ingresar datos a un archivo de texto lo hacemos asi:
+    printf "$Total \n" >>$File1    o así    `printf "%i %i %i\n" $temp1 >> $file1`
+
+En el __ejercicio 1__ leemos desde la segunda linea indicando el separador ":"
+
+	Suma=$(cat $i | tail -n+2 | awk -F ':'
+
+En los archivos executionSummary-NNN.txt los tiempos de simulación estás en la posición 6, 7 y 8. Entonces se sumas así: suma=$6+$7+$8
+La memoria del simulador esta en la posición $9.
+
+Para el __ejercicio 2__ en los archivos archivos summary-NNN.txt los tiempos de evacuación están en $8.
+Además el indicador de modelo de persona está en $3 y el indicador de grupo etario está en $4.
+
+En el __ejercicio 3__ en los archivos usePhone-NNN.txt el número de personas que ocupan el teléfono movil están en $3
+en los tiempo de medición $2.
--- stats.sh
+++ stats.sh
@@ -26,16 +26,18 @@ File2="Memoria.txt"
 executionSummaryFiles=(`find $searchDir -name '*executionSummary-*.txt' -print | sort | grep -v '._'`)
 for i in ${executionSummaryFiles[*]};
 do
-	Total=$(cat $i | tail -n+2 | awk -F ':' 'BEGIN{sum=0}{sum=$6+$7+$8} END{print sum}')
+	Total=$(cat $i | tail -n+2 | awk -F ':' 'BEGIN{sum=0}{sum=$6+$7+$8} END{print sum}') #Suma de los tiempos
 	printf "$Total \n" >>$File1
-	tsimTotal=$(cat $File1 | awk 'BEGIN{min=2**63-1; max=0}{if($File1<min){min=$File1};\
+	#Calculo de las sumas totales, promedio, mínimos y máximos
+	tsimTotal=$(cat $File1 | awk 'BEGIN{min=2**63-1; max=0}{if($File1<min){min=$File1};\ 
 												if($File1>max){max=$File1};\
 													total+=$File1; count+=1;\
 													} \
 													END{ print total, total/count, min, max }')
 
-	Suma=$(cat $i | tail -n+2 | awk -F ':' 'BEGIN{suma=0}{suma=$10} END{print suma}')
+	Suma=$(cat $i | tail -n+2 | awk -F ':' 'BEGIN{suma=0}{suma=$9} END{print suma}') #Suma de la memoria de simulación
 	printf "$Suma \n" >>$File2
+	#Calculo de las sumas totales, promedio, mínimos y máximos
 	memUsed=$(cat $File2 | awk 'BEGIN{ min=2**63-1; max=0}{if($File2<min){min=$File2};\
 													if($File2>max){max=$File2};\
 														suma+=$File2; count+=1;\
@@ -43,115 +45,220 @@ do
 														 END{print suma, suma/count, min, max}')
 done
 
-printf "%i:%i:%i:%i\n%i:%i:%i:%i\n" $tsimTotal $memUsed >> $OUTFILE
-rm Tiempo.txt
-rm Memoria.txt
+printf "tsimTotal:%i:%i:%i:%i\n" $tsimTotal >> $OUTFILE
+printf "memUsed:%i:%i:%i:%i\n" $memUsed >> $OUTFILE
+rm Tiempo.txt Memoria.txt
 
 
 ######## Ejercicio 2 ##################################################################################################
-OutFile="evacuation.txt"
 
-summaryFiles=(`find $searchDir -name '*Summary-*.txt' -print | sort `)
+OutFile="evacuation.txt"
+file1="File1.txt"
+file2="File2.txt"
+file3="File3.txt"
+file4="File4.txt"
+file5="File5.txt"
+file6="File6.txt"
+file7="File7.txt"
+file8="File8.txt"
+file9="File9.txt"
+file10="File10.txt"
+file11="File11.txt"
+
+summaryFiles=(`find $searchDir -name '*Summary-*.txt' -print | sort | grep -v '._'`)
 for i in ${summaryFiles[*]};
 do
-	alls=$( cat $i| tail -n+2 | awk -F ':' 'BEGIN{ min=2**63-1; max=0 }{if($8<min){min=$8};\
+	#Para todas las Personas
+	temp1=$(cat $i | tail -n+2 | awk -F ':' 'BEGIN{ min=2**63-1; max=0 }{if($8<min){min=$8};\
 											if($8>max){max=$8};\
 											total+=$8; count+=1;\
+											} \
+											END{ print total,min,max }')
+	printf "%i %i %i\n" $temp1 >> $file1
+	alls=$(cat $file1 | awk 'BEGIN{ min=2**63-1; max=0 }{if($2<min){min=$2};\
+											if($3>max){max=$3};\
+											total+=$1; count+=1;\
+											} \
+											END{ print total/count,min,max }')
+	
+	#Solo Residentes
+	temp2=$( cat $i | tail -n+2 | awk -F ':' 'BEGIN{ min=2**63-1; max=0 }{if($3==0){if($8<min){min=$8};\
+											if($8>max){max=$8};\
+											total+=$8; countR+=1;\
+											}} \
+											END{ print total,min,max }')
+	printf "%i %i %i\n" $temp2 >> $file2
+	residents=$(cat $file2 | awk 'BEGIN{ min=2**63-1; max=0 }{if($2<min){min=$2};\
+											if($3>max){max=$3};\
+											total+=$1; count+=1;\
 											} \
 											END{ print total/count,min,max }')
 
-	residents=$( cat $i | tail -n+2 | awk -F ':' '$3 == 0' 'BEGIN{ min=2**63-1; max=0 }{if($8<min){min=$8};\
+	#Visitantes tipo 1
+	temp3=$( cat $i | tail -n+2 | awk -F ':' 'BEGIN{ min=2**63-1; max=0 }{if($3==1){if($8<min){min=$8};\
 											if($8>max){max=$8};\
-											total+=$8; count+=1;\
-											} \
-											END{ print total,total/count,min,max }')
-
-	visitorsI=$( cat $i | tail -n+2 | awk -F ':' '$3 == 1' 'BEGIN{ min=2**63-1; max=0 }{if($8<min){min=$8};\
-											if($8>max){max=$8};\
-											total+=$8; count+=1;\
-											} \
-											END{ print total/count,min,max }')
-
-	residents-G0=$(cat $i | tail -n+2 | awk -F ':' '$3 == 0' | awk -F ':' '$4 == 0' 'BEGIN{ min=2**63-1; max=0 }{if($8<min){min=$8};\
-											if($8>max){max=$8};\
-											total+=$8; count+=1;\
+											total+=$8; countR+=1;\
+											}} \
+											END{ print total,min,max }')
+	printf "%i %i %i\n" $temp3 >> $file3
+	visitorsI=$(cat $file3 | awk 'BEGIN{ min=2**63-1; max=0 }{if($2<min){min=$2};\
+											if($3>max){max=$3};\
+											total+=$1; count+=1;\
 											} \
 											END{ print total/count,min,max }')
 
-	residents-G1=$(cat $i | tail -n+2 | awk -F ':' '$3 == 0' | awk -F ':' '$4 == 1' 'BEGIN{ min=2**63-1; max=0 }{if($8<min){min=$8};\
+	#Residentes menores de 15 años
+	temp4=$( cat $i | tail -n+2 | awk -F ':' 'BEGIN{ min=2**63-1; max=0 }{if($3==0 && $4 == 0){if($8<min){min=$8};\
 											if($8>max){max=$8};\
-											total+=$8; count+=1;\
+											total+=$8; countR+=1;\
+											}} \
+											END{ print total,min,max }')
+	printf "%i %i %i\n" $temp4 >> $file4
+	residentsG0=$(cat $file4 | awk 'BEGIN{ min=2**63-1; max=0 }{if($2<min){min=$2};\
+											if($3>max){max=$3};\
+											total+=$1; count+=1;\
 											} \
 											END{ print total/count,min,max }')
 
-	residents-G2=$(cat $i | tail -n+2 | awk -F ':' '$3 == 0' | awk -F ':' '$4 == 2' 'BEGIN{ min=2**63-1; max=0 }{if($8<min){min=$8};\
+	#Residentes desde 15 hasta 29 años
+	temp5=$( cat $i | tail -n+2 | awk -F ':' 'BEGIN{ min=2**63-1; max=0 }{if($3==0 && $4 == 1){if($8<min){min=$8};\
 											if($8>max){max=$8};\
-											total+=$8; count+=1;\
+											total+=$8; countR+=1;\
+											}} \
+											END{ print total,min,max }')
+	printf "%i %i %i\n" $temp5 >> $file5
+	residentsG1=$(cat $file5 | awk 'BEGIN{ min=2**63-1; max=0 }{if($2<min){min=$2};\
+											if($3>max){max=$3};\
+											total+=$1; count+=1;\
 											} \
 											END{ print total/count,min,max }')
 
-	residents-G3=$(cat $i | tail -n+2 | awk -F ':' '$3 == 0' | awk -F ':' '$4 == 3' 'BEGIN{ min=2**63-1; max=0 }{if($8<min){min=$8};\
+	#Resitentes desde 30 hasta 64 años
+	temp6=$( cat $i | tail -n+2 | awk -F ':' 'BEGIN{ min=2**63-1; max=0 }{if($3==0 && $4 == 2){if($8<min){min=$8};\
 											if($8>max){max=$8};\
-											total+=$8; count+=1;\
+											total+=$8; countR+=1;\
+											}} \
+											END{ print total,min,max }')
+	printf "%i %i %i\n" $temp6 >> $file6
+	residentsG2=$(cat $file6 | awk 'BEGIN{ min=2**63-1; max=0 }{if($2<min){min=$2};\
+											if($3>max){max=$3};\
+											total+=$1; count+=1;\
 											} \
 											END{ print total/count,min,max }')
 
-	visitorsI-G0=$(cat $i | tail -n+2 | awk -F ':' '$3 == 1' | awk -F ':' '$4 == 0' 'BEGIN{ min=2**63-1; max=0 }{if($8<min){min=$8};\
+	#Residentes mayores a 64 años
+	temp7=$( cat $i | tail -n+2 | awk -F ':' 'BEGIN{ min=2**63-1; max=0 }{if($3==0 && $4 == 3){if($8<min){min=$8};\
 											if($8>max){max=$8};\
-											total+=$8; count+=1;
+											total+=$8; countR+=1;\
+											}} \
+											END{ print total,min,max }')
+	printf "%i %i %i\n" $temp7 >> $file7
+	residentsG1=$(cat $file7 | awk 'BEGIN{ min=2**63-1; max=0 }{if($2<min){min=$2};\
+											if($3>max){max=$3};\
+											total+=$1; count+=1;\
 											} \
 											END{ print total/count,min,max }')
 
-	visitorsI-G1=$(cat $i | tail -n+2 | awk -F ':' '$3 == 1' | awk -F ':' '$4 == 1' 'BEGIN{ min=2**63-1; max=0 }{if($8<min){min=$8};\
+	#Visitantes tipo 1 menores de 14 años
+	temp8=$( cat $i | tail -n+2 | awk -F ':' 'BEGIN{ min=2**63-1; max=0 }{if($3==1 && $4 == 0){if($8<min){min=$8};\
 											if($8>max){max=$8};\
-											total+=$8; count+=1;\
+											total+=$8; countR+=1;\
+											}} \
+											END{ print total,min,max }')
+	printf "%i %i %i\n" $temp8 >> $file8
+	visitorsIG0=$(cat $file8 | awk 'BEGIN{ min=2**63-1; max=0 }{if($2<min){min=$2};\
+											if($3>max){max=$3};\
+											total+=$1; count+=1;\
 											} \
 											END{ print total/count,min,max }')
 
-	visitorsI-G2=$(cat $i | tail -n+2 | awk -F ':' '$3 == 1' | awk -F ':' '$4 == 2' 'BEGIN{ min=2**63-1; max=0 }{if($8<min){min=$8};\
+	#Visitantes tipo 1 desde 15 hasta 29 años
+	temp9=$( cat $i | tail -n+2 | awk -F ':' 'BEGIN{ min=2**63-1; max=0 }{if($3==1 && $4 == 1){if($8<min){min=$8};\
 											if($8>max){max=$8};\
-											total+=$8; count+=1;\
+											total+=$8; countR+=1;\
+											}} \
+											END{ print total,min,max }')
+	printf "%i %i %i\n" $temp9 >> $file9
+	visitorsIG1=$(cat $file9 | awk 'BEGIN{ min=2**63-1; max=0 }{if($2<min){min=$2};\
+											if($3>max){max=$3};\
+											total+=$1; count+=1;\
 											} \
 											END{ print total/count,min,max }')
 
-	visitorsI-G3=$(cat $i | tail -n+2 | awk -F ':' '$3 == 1' | awk -F ':' '$4 == 3' 'BEGIN{ min=2**63-1; max=0 }{if($8<min){min=$8};\
+	##Visitantes tipo 1 desde 30 hasta 64 años
+	temp10=$( cat $i | tail -n+2 | awk -F ':' 'BEGIN{ min=2**63-1; max=0 }{if($3==1 && $4 == 2){if($8<min){min=$8};\
 											if($8>max){max=$8};\
-											total+=$8; count+=1;\
+											total+=$8; countR+=1;\
+											}} \
+											END{ print total,min,max }')
+	printf "%i %i %i\n" $temp10 >> $file10
+	visitorsIG2=$(cat $file10 | awk 'BEGIN{ min=2**63-1; max=0 }{if($2<min){min=$2};\
+											if($3>max){max=$3};\
+											total+=$1; count+=1;\
+											} \
+											END{ print total/count,min,max }')
+
+	#Visitantes mayores a 64 años
+	temp11=$( cat $i | tail -n+2 | awk -F ':' 'BEGIN{ min=2**63-1; max=0 }{if($3==1 && $4 == 3){if($8<min){min=$8};\
+											if($8>max){max=$8};\
+											total+=$8; countR+=1;\
+											}} \
+											END{ print total,min,max }')
+	printf "%i %i %i\n" $temp11 >> $file11
+	visitorsIG3=$(cat $file11 | awk 'BEGIN{ min=2**63-1; max=0 }{if($2<min){min=$2};\
+											if($3>max){max=$3};\
+											total+=$1; count+=1;\
 											} \
 											END{ print total/count,min,max }')
 done
 
-printf "alls:%i:%i:%i\n" $alls >> $OutFiles
+printf "alls:%i:%i:%i\n" $alls >> $OutFile
 printf "residents:%i:%i:%i\n" $residents >> $OutFile
 printf "visitorsI:%i:%i:%i\n" $visitorsI >> $OutFile
-printf "residents-G0:%i:%i:%i\n" $residents-G0 >> $OutFile
-printf "residents-G1:%i:%i:%i\n" $residents-G1 >> $OutFile
-printf "residents-G2:%i:%i:%i\n" $residents-G2 >> $OutFile
-printf "residents-G3:%i:%i:%i\n" $residents-G3 >> $OutFile
-printf "visitorsI-G0:%i:%i:%i\n" $visitorsI-G0 >> $OutFile
-printf "visitorsI-G1:%i:%i:%i\n" $visitorsI-G1 >> $OutFile
-printf "visitorsI-G2:%i:%i:%i\n" $visitorsI-G2 >> $OutFile
-printf "visitorsI-G3:%i:%i:%i\n" $visitorsI-G3 >> $OutFile
+printf "residents-G0:%i:%i:%i\n" $residentsG0 >> $OutFile
+printf "residents-G1:%i:%i:%i\n" $residentsG1 >> $OutFile
+printf "residents-G2:%i:%i:%i\n" $residentsG2 >> $OutFile
+printf "residents-G3:%i:%i:%i\n" $residentsG3 >> $OutFile
+printf "visitorsI-G0:%i:%i:%i\n" $visitorsIG0 >> $OutFile
+printf "visitorsI-G1:%i:%i:%i\n" $visitorsIG1 >> $OutFile
+printf "visitorsI-G2:%i:%i:%i\n" $visitorsIG2 >> $OutFile
+printf "visitorsI-G3:%i:%i:%i\n" $visitorsIG3 >> $OutFile
+
+rm File1.txt File2.txt File3.txt File4.txt File5.txt File6.txt File7.txt File8.txt File9.txt File10.txt File11.txt
+
 
 
 ######## Ejercicio 3 ##################################################################################################
-usePhoneFiles=(`find $searchDir -name '*usePhone-*.txt' -print | sort `)
 
-File3="UsePhone.txt"
 OutFiles="usePhone-stats.txt"
+File3="tiempo_medicion.txt"
+File4="UsePhone.txt"
 
-for i in ${usePhoneFiles[*]};
+printf "timestamp:promedio:min:max \n" >> $OutFilePhone
+archivo_usePhone=(`find $searchDir -name '*usePhone-*.txt' -print | sort | grep -v '._'`)
+
+for i in ${archivo_usePhone[*]};
 do
+	
+	#Calcula promedios del tiempo medido
+	Tiempo_medicion=(`cat $i | tail -n+3 | cut -d ':' -f 2`) #Las primeras 2 lineas del archivo no se leen
+	for j in ${tiempo_medicion[*]};
+	do
+		printf "%i\n" $j >> $File3
+		Tiempomedicion=$(cat $File3 | awk 'BEGIN{suma=0}{suma+=$1; count+=1 }; END { print suma/count }')
+	done
+
+	#Calcula promedio, min y max del uso del celular
 	UsePhone=(`cat $i | tail -n+3 | cut -d ':' -f 3`)
 	for j in ${UsePhone[*]};
 	do
-		printf "%d:\n" $j >> $File3
-		#Calcula promedio, min y max
-		Phone=$(cat $File3 | cut -d ':' -f 1 | awk 'BEGIN{ min=2**63-1; max=0}{if($j<min){min=$j}};\
-															{if($j>max){max=$j}};\
-															{total+=$j; count+=1};\
-															END { print total/count, min, max}')
+		printf "%i\n" $j >> $File4
+		Phone=$(cat $File4 awk 'BEGIN{ min=2**63-1; max=0}{if($j<min){min=$j}};\
+														{if($j>max){max=$j}};\
+														{total+=$j; count+=1};\
+														END { print total/count, min, max}')
 	done
-	printf "timestamp:%i:%i:%i \n" $Phone >> $OutFiles
+	printf "%i:" $Tiempomedicion >> $OutFiles
+	printf "%i:%i:%i\n" $Phone >> $OutFiles
 done
-rm UsePhone.txt
+rm tiempo_medicion.txt UsePhone.txt 
